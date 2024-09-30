82ecc3a6f2bf655a1f2c7fe59dac6f22255f4816
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2024-09-30 12:38:59 +1000

    Trying to go back to napi-rs

--- next.config.js
+++ next.config.js
@@ -16,5 +16,18 @@ module.exports = {
         pathname: '**'
       }
     ]
+  },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /pdf\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto'
+    })
+    if (isServer) {
+      config.externals.push({
+        '@napi-rs/canvas': 'commonjs @napi-rs/canvas'
+      })
+    }
+    return config
   }
 }
