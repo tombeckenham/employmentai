8d031ea2a172572027b3c5157f37cebaab6caa00
Tom Beckenham <34339192+tombeckenham@users.noreply.github.com>
2024-09-30 14:54:08 +1000

    Trying to explicitly set runtime to nodejs

--- app/api/process-job/route.ts
+++ app/api/process-job/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest } from 'next/server'
 import { verifySignatureAppRouter } from '@upstash/qstash/nextjs'
 import { processJob } from '@/lib/processJobs'
+export const runtime = 'nodejs' // Ensure Node.js runtime is used
 
 async function handler(req: NextRequest) {
   const body = await req.json()
